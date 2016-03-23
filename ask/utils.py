2bb2ad41088d31421430fd79d665550646b49c8d
Joe Germuska <joe@germuska.com>
2016-03-23 18:06:57 -0500

    support easier access to session attributes in the request, and for setting them in the response builder

--- ask/utils.py
+++ ask/utils.py
@@ -16,19 +16,19 @@ RAW_RESPONSE = """
     }
 }"""
 
-    
+
 
 class Request(object):
     """
     Simple wrapper around the JSON request
     received by the module
-    """    
+    """
     def __init__(self, request_dict):
         self.request = request_dict
         self.metadata = {}
         if self.intent_name():
             self.slots = self.get_slot_map()
-        
+
     def request_type(self):
         return self.request["request"]["type"]
 
@@ -41,7 +41,7 @@ class Request(object):
         if self.intent_name() == None:
             return False
         return True
-    
+
     def user_id(self):
         return self.request["session"]["user"]["userId"]
 
@@ -54,6 +54,10 @@ class Request(object):
     def session_id(self):
         return self.request["session"]["sessionId"]
 
+    def session(self):
+        """Return the session attributes which are currently set."""
+        return self.request["session"]["sessionAttributes"]
+
     def get_slot_value(self, slot_name):
         try:
             return self.request["request"]["intent"]["slots"][slot_name]["value"]
@@ -64,25 +68,26 @@ class Request(object):
     def get_slot_names(self):
         try:
             return self.request['request']['intent']['slots'].keys()
-        except:            
+        except:
             return []
 
     def get_slot_map(self):
         return {slot_name : self.get_slot_value(slot_name) for slot_name in self.get_slot_names()}
 
-    
+
 class ResponseBuilder(object):
     """
     Simple class to help users to build responses
     """
     base_response = eval(RAW_RESPONSE)
-       
+
     @classmethod
-    def create_response(self, message=None, end_session=False, card_obj=None, 
-                        reprompt_message=None, is_ssml=None):
+    def create_response(self, message=None, end_session=False, card_obj=None,
+                        reprompt_message=None, session_attributes=None, is_ssml=None):
         """
         message - text message to be spoken out by the Echo
         end_session - flag to determine whether this interaction should end the session
+        session_attributes - a dictionary of values to be sent in the response which will come back with the next request in this session
         card_obj = JSON card object to substitute the 'card' field in the raw_response
         """
         response = self.base_response
@@ -93,8 +98,10 @@ class ResponseBuilder(object):
             response['response']['card'] = card_obj
         if reprompt_message:
             response['response']['reprompt'] = self.create_speech(reprompt_message, is_ssml)
+        if session_attributes:
+            response['sessionAttributes'] = session_attributes
         return response
-    
+
     @classmethod
     def create_speech(cls, message=None, is_ssml=False):
         data = {}
@@ -110,7 +117,7 @@ class ResponseBuilder(object):
     def create_card(self, title=None, subtitle=None, content=None, card_type="Simple"):
         """
         card_obj = JSON card object to substitute the 'card' field in the raw_response
-        format: 
+        format:
         {
           "type": "Simple", #COMPULSORY
           "title": "string", #OPTIONAL
@@ -127,8 +134,8 @@ class ResponseBuilder(object):
 
 class VoiceHandler(ResponseBuilder):
     """    Decorator to store function metadata
-    Functions that are annotated with this label are 
-    treated as voice handlers """  
+    Functions that are annotated with this label are
+    treated as voice handlers """
 
     def __init__(self):
         self._handlers = { "IntentRequest" : {} }
@@ -138,29 +145,29 @@ class VoiceHandler(ResponseBuilder):
         ''' Decorator to register default handler '''
         def _handler(func):
             self._handlers[self._default] = func
-            return func        
+            return func
         return _handler
-    
+
     def intent_handler(self, intent):
         ''' Decorator to register intent handler'''
         def _handler(func):
             self._handlers['IntentRequest'][intent] = func
         return _handler
 
-    
+
     def request_handler(self, request_type):
         ''' Decorator to register generic request handler '''
         def _handler(func):
             self._handlers[request_type] = func
         return _handler
-    
-            
+
+
     def route_request(self, request_json, metadata={}):
         ''' Route the request object to the right handler function '''
-        
+
         request = Request(request_json)
         request.meta_data = metadata
-        
+
         handler_fn = self._handlers[self._default] # Set default handling for noisy requests
 
         if not request.is_intent() and (request.request_type() in self._handlers):
@@ -172,5 +179,3 @@ class VoiceHandler(ResponseBuilder):
             handler_fn = self._handlers['IntentRequest'][request.intent_name()]
 
         return handler_fn(request)
-
-
